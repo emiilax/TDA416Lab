63860ce7827a9ea442bf1aa66a3536cca278545f
emiilax <e.axelsson@hotmail.se>
2016-02-25 14:39:08 +0100

    added all given files from lab4

---
+++ lab4/src/ting/DirectedGraph.java
@@ -0,0 +1,27 @@
+package ting;
+
+import ting.givna_filer.Edge;
+
+import java.util.Iterator;
+
+public class DirectedGraph<E extends Edge> {
+
+
+	public DirectedGraph(int noOfNodes) {
+		;
+	}
+
+	public void addEdge(E e) {
+		;
+	}
+
+	public Iterator<E> shortestPath(int from, int to) {
+		return null;
+	}
+		
+	public Iterator<E> minimumSpanningTree() {
+		return null;
+	}
+
+}
+  
---
+++ lab4/src/ting/givna_filer/BusStop.java
@@ -0,0 +1,14 @@
+package ting.givna_filer;
+
+public class BusStop extends NodeObject {
+
+  int xpos, ypos;
+  
+  BusStop(String name, int xpos, int ypos) {
+    super(name);
+    this.xpos = xpos;
+    this.ypos = ypos;
+  }
+
+}
+       
